e8cf234f578d06149496b56379813456293ecdbf
JeanCharles14 <jeancduasan@gmail.com>
2022-12-26 12:22:34 -0300

    animação com icones

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Feather } from "react-native-vector-icons"
+import LoteView from 'lottie-react-native'
+import React from 'react';
+import anime from "./assets/check.json";
 
 export default function App() {
+
+  let animation = React.createRef()
+
+  function startAnimaton() {
+    animation.current.play()
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={startAnimaton}>
+        <Feather name="play" size={60} color="red" />
+
+      </TouchableOpacity>
+      <LoteView
+        source={anime}
+        style={
+          {
+            width: 200,
+            height: 200,
+          }
+        }
+        loop={false}
+        ref={animation}
+      />
+      <Button title='Start Animation' onPress={startAnimaton} />
       <StatusBar style="auto" />
     </View>
   );
